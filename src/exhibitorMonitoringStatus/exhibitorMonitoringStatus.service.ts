649ffacecaf6ecf5570c8fb356b1e148d5d8687b
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-25 21:18:12 +0100

    ajout relation exhibitorMonitoring

--- src/exhibitorMonitoringStatus/exhibitorMonitoringStatus.service.ts
+++ src/exhibitorMonitoringStatus/exhibitorMonitoringStatus.service.ts
@@ -1,28 +1,29 @@
-import {Inject, Injectable} from "@nestjs/common";
-import {Repository} from "typeorm";
-import {ExhibitorMonitoringStatus} from "./exhibitorMonitoringStatus.entity";
-import {ExhibitorMonitoringStatusDto} from "./exhibitorMonitoringStatus.dto";
+import { Inject, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { ExhibitorMonitoringStatus } from "./exhibitorMonitoringStatus.entity";
+import { ExhibitorMonitoringStatusDto } from "./exhibitorMonitoringStatus.dto";
 
 @Injectable()
 export class ExhibitorMonitoringStatusService {
 
-    @Inject("EXHIBITOR_MONITORING_STATUS_REPOSITORY")
-    private readonly exhibitorMonitoringStatusRepository: Repository<ExhibitorMonitoringStatus>
+  @Inject("EXHIBITOR_MONITORING_STATUS_REPOSITORY")
+  private readonly exhibitorMonitoringStatusRepository: Repository<ExhibitorMonitoringStatus>;
 
-    async create(exhibitorMonitoringStatusDto: ExhibitorMonitoringStatusDto) {
+  async create(exhibitorMonitoringStatusDto: ExhibitorMonitoringStatusDto) {
 
-        const exhibitorMonitoringStatus = new ExhibitorMonitoringStatus();
+    const exhibitorMonitoringStatus = new ExhibitorMonitoringStatus();
 
-        exhibitorMonitoringStatus.label = exhibitorMonitoringStatusDto.label;
+    exhibitorMonitoringStatus.label = exhibitorMonitoringStatusDto.label;
 
-        return this.exhibitorMonitoringStatusRepository.save(exhibitorMonitoringStatus);
-    }
+    return this.exhibitorMonitoringStatusRepository.save(exhibitorMonitoringStatus);
+  }
 
-    async getAll(): Promise<ExhibitorMonitoringStatus[]> {
-        return this.exhibitorMonitoringStatusRepository.find({
-            order: {
-                label: "ASC"
-            }
-        })
-    }
+  async getAll(): Promise<ExhibitorMonitoringStatus[]> {
+    return this.exhibitorMonitoringStatusRepository.find({
+      order: {
+        label: "ASC"
+      }
+    });
+  }
+
 }
