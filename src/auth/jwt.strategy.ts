844354136bc4cf6fe21ba84cce2fbf24a566808a
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-26 21:52:32 +0100

    Environment variable GCS and jwt key

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,21 +1,25 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { jwtConstants } from './constants';
 import {UserService} from "../user/user.service";
 import { User } from "../user/user.entity";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: configService.get<string>("SECRET"),
     });
   }
 
   async validate(payload: any) {
+    console.log(this.configService.get<string>("SECRET"))
     const user : User = await this.userService.getById(payload.id)
     const {password, ...result} = user
     return { ...result};
--- src/utils/gcs.ts
+++ src/utils/gcs.ts
@@ -3,5 +3,6 @@ import * as path from "path";
 
 export const gcs: Storage = new Storage({
     keyFilename: path.join(__dirname, "../../storage.json"),
-    projectId: "festival-jeu"
+    projectId: "festival-jeu",
+    //credentials: JSON.parse(process.env.GCS_KEYFILE)
 })
