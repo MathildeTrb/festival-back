1fdad2cf23a8ce62d92ad3fd25563980b606ef7a
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2021-03-31 16:13:10 +0200

    maj table remaining

--- src/reservationDetails/reservationDetails.service.ts
+++ src/reservationDetails/reservationDetails.service.ts
@@ -4,11 +4,13 @@ import { ReservationDetailsDto } from "./reservationDetails.dto";
 import { ReservationDetails } from "./reservationDetails.entity";
 import { Repository } from "typeorm";
 import {InjectRepository} from "@nestjs/typeorm";
+import { SpaceService } from "../space/space.service";
 
 @Injectable()
 export class ReservationDetailsService{
 
   constructor(
+    private readonly spaceService: SpaceService,
     @InjectRepository(ReservationDetails)
     private reservationDetailsRepository: Repository<ReservationDetails>
   ) {
@@ -21,6 +23,12 @@ export class ReservationDetailsService{
     reservationDetails.tableReserved = newReservationDetails.tableReserved
     reservationDetails.reservation = savedReservation
     reservationDetails.space = newReservationDetails.space
+
+    const newTableRemaining = newReservationDetails.space.tableRemaining - (
+      reservationDetails.meterReserved * 6 + reservationDetails.tableReserved
+    )
+
+    await this.spaceService.updateTableRemaining(newReservationDetails.space, newTableRemaining)
 
     return await this.reservationDetailsRepository.save(reservationDetails);
   }
--- src/space/space.service.ts
+++ src/space/space.service.ts
@@ -1,33 +1,37 @@
-import {Inject, Injectable} from "@nestjs/common";
-import {Repository} from "typeorm";
-import {Space} from "./space.entity";
-import {Festival} from "../festival/festival.entity";
-import {SpaceDto} from "./space.dto";
-import {InjectRepository} from "@nestjs/typeorm";
+import { Inject, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Space } from "./space.entity";
+import { Festival } from "../festival/festival.entity";
+import { SpaceDto } from "./space.dto";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class SpaceService {
-    constructor(
-        @InjectRepository(Space)
-        private spaceRepository: Repository<Space>
-    ) {
-    }
+  constructor(
+    @InjectRepository(Space)
+    private spaceRepository: Repository<Space>
+  ) {
+  }
 
 
-    async create(festival: Festival, newSpace: SpaceDto) {
-        const space: Space = Space.createFromDto(festival, newSpace)
-        return this.spaceRepository.save(space)
-    }
+  async create(festival: Festival, newSpace: SpaceDto) {
+    const space: Space = Space.createFromDto(festival, newSpace);
+    return this.spaceRepository.save(space);
+  }
 
-    async getAll(id: number) {
-        return this.spaceRepository.find({
-            where: {
-                festival: id
-            },
-            order: {
-                label: "ASC"
-            }
-        })
-    }
+  async getAll(id: number) {
+    return this.spaceRepository.find({
+      where: {
+        festival: id
+      },
+      order: {
+        label: "ASC"
+      }
+    });
+  }
+
+  async updateTableRemaining(space : Space, tableRemaining: number) {
+      return this.spaceRepository.update({id : space.id}, {tableRemaining : tableRemaining})
+  }
 }
 
